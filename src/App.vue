c7768504740ecccff77812afe103f9f924a3691a
Virginie <vb@ikimo9.com>
2023-02-12 19:09:48 +0100

    add with and height on img tag

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <img alt="Vue logo" src="./assets/logo.png" width="150px" height="150px" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
